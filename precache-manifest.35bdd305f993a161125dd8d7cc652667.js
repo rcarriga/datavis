e368522a038112894c7664d0dfc7a053c3da150f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-19 15:51:17 +0000

    Deploy rcarriga/datavis to github.com/rcarriga/datavis.git:gh-pages

---
+++ precache-manifest.35bdd305f993a161125dd8d7cc652667.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e4634e1994188f8042779e46ddf16c6",
+    "url": "/datavis/index.html"
+  },
+  {
+    "revision": "4524e6bb8aebe8d1b706",
+    "url": "/datavis/static/css/2.4edb0015.chunk.css"
+  },
+  {
+    "revision": "eee2de20394a3057fc16",
+    "url": "/datavis/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4524e6bb8aebe8d1b706",
+    "url": "/datavis/static/js/2.78a0e238.chunk.js"
+  },
+  {
+    "revision": "51034d186cf3f398b269435ccb0ae13a",
+    "url": "/datavis/static/js/2.78a0e238.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eee2de20394a3057fc16",
+    "url": "/datavis/static/js/main.f6f2a9dd.chunk.js"
+  },
+  {
+    "revision": "6b8be1a799057be28cb0",
+    "url": "/datavis/static/js/runtime-main.a7cced7b.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/datavis/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/datavis/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/datavis/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/datavis/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/datavis/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
